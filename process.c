300de76841543c16a73db60b9e6cf413a371472b
SurrealArray <42991521+SurrealArray@users.noreply.github.com>
2020-09-19 11:59:41 -0600

    Update process.c

--- process.c
+++ process.c
@@ -1,12 +1,3 @@
-/*
- * Omar Beltran 
- * RedID: 818036447
- * cssc2146
- * CS 570, Summer 2020
- * Assignment #2, Microshell
- * process.c
- */
-
 #include "process.h"
 
 int shellProcess(int fileDesc, int cmd){
