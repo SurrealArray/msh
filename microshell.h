3cc69fc2563cebacfe86d11a3e68725fd92e4f4c
SurrealArray <42991521+SurrealArray@users.noreply.github.com>
2020-09-19 11:58:50 -0600

    Update microshell.h

--- microshell.h
+++ microshell.h
@@ -1,12 +1,3 @@
-/*
- * Omar Beltran 
- * RedID: 818036447
- * cssc2146
- * CS 570, Summer 2020
- * Assignment #2, Microshell
- * msh.h
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
